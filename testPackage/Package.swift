6ab6250acbf6ebd8415e79e6c8d047dd463d41c3
Bilal Mughal <bilal@cinqtech.com>
2024-08-03 15:21:17 +0500

    updated.

--- testPackage/Package.swift
+++ testPackage/Package.swift
@@ -11,17 +11,18 @@ let package = Package(
     
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
+        
         .library(
             name: "testPackage",
             targets: ["testPackage"]),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        
         .target(
-            name: "testPackage",
-            dependencies: [],
-            path: "testPackage/sources/testPackage"),
+            name: "testPackage"),
         
         .testTarget(
             name: "testPackageTests",
