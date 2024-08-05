5305e3767239628df1a8cf42d96f51d22719fde5
Bilal Mughal <bilal@cinqtech.com>
2024-08-05 13:20:41 +0500

    version: 1.0.1

--- Sources/testPackage/testPackage.swift
+++ Sources/testPackage/testPackage.swift
@@ -7,30 +7,34 @@ import WebKit
 
 public class MediumWebView: WKWebView {
     
-    public init() {
-        let webConfiguration = WKWebViewConfiguration()
-        webConfiguration.preferences.javaScriptEnabled = true
-        super.init(frame: .zero, configuration: webConfiguration)
-        self.scrollView.isScrollEnabled = false
-        self.isMultipleTouchEnabled = false
+    public func start() {
         print("version: \(info.version)")
     }
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-
-    @discardableResult
-    public func load() -> WKNavigation? {
-        if let url = URL(string:"http://google.com") {
-            let req = URLRequest(url: url)
-            return super.load(req)
-        }
-        return nil
-    }
+//    public init() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webConfiguration.preferences.javaScriptEnabled = true
+//        super.init(frame: .zero, configuration: webConfiguration)
+//        self.scrollView.isScrollEnabled = false
+//        self.isMultipleTouchEnabled = false
+//        print("version: \(info.version)")
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//    }
+//
+//    @discardableResult
+//    public func load() -> WKNavigation? {
+//        if let url = URL(string:"http://google.com") {
+//            let req = URLRequest(url: url)
+//            return super.load(req)
+//        }
+//        return nil
+//    }
 }
 
 
 struct info {
-    static var version = "0.0.1"
+    static var version = "1.0.1"
 }
